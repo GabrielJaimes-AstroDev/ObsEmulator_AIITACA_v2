be6c2ece3728244934aa117c4d6386580e7a3d70
Gabriel Jaimes <bolivianvirtualobservatory@gmail.com>
2026-04-16 11:05:00 +0200

    Update 7.OBSEMULATOR_Streamlit_WEB_v1.py

--- 7.OBSEMULATOR_Streamlit_WEB_v1.py
+++ 7.OBSEMULATOR_Streamlit_WEB_v1.py
@@ -43,7 +43,7 @@ except Exception:
 DEFAULT_MERGED_H5 = ""
 DEFAULT_NOISE_NN_H5 = ""
 DEFAULT_FILTER_FILE = ""
-DEFAULT_GDRIVE_MODELS_LINK = "https://drive.google.com/drive/folders/1Zm3UpfWfXfa-Uh1sc1HBH3o25qYvqMNH?usp=drive_link"
+DEFAULT_GDRIVE_MODELS_LINK = "https://drive.google.com/drive/folders/1rkSAT8Dp5Zo5HM6S2VMeAPPRYw4asz1v?usp=drive_link"
 
 DEFAULT_TARGET_FREQS = [
 	84.299,
